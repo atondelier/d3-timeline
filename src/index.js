7e7d2656ba6d3d441c69f39462fd62721245627e
lib3d <atondelier@gmail.com>
2015-08-03 00:05:14 +0200

    add options in example

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ var debugOptions = {
 };
 
 var gui = new dat.GUI({
-    width: 300
+    width: 400
 });
 
 var debugGui = gui.addFolder('Debug');
@@ -55,7 +55,10 @@ debugGui.add(debugOptions, 'outlineGroups').name('Outline groups').onChange(func
     $(document.body).toggleClass('debug-outlineGroups', debugOptions.outlineGroups);
 });
 
-var timelineGui = gui.addFolder('Timeline options');
+var timelineGui = gui.addFolder('Rendering');
+var dimensionsGui = gui.addFolder('Dimensions');
+var cullingGui = gui.addFolder('Culling');
+var elementGui = gui.addFolder('Text alignment');
 
 function forceFullRedraw() {
     timeline.container.selectAll('g.timelineElement').remove();
@@ -67,13 +70,20 @@ function forceFullRedraw() {
 timelineGui.add(timeline.options, 'renderOnIdle').name('Render on idle');
 timelineGui.add(timeline.options, 'hideTicksOnDrag').name('Hide ticks on drag');
 timelineGui.add(timeline.options, 'hideTicksOnZoom').name('Hide ticks on zoom');
-timelineGui.add(timeline.options, 'cullingTolerance', -10, 10).step(1).name('Culling distance');
-timelineGui.add(timeline.options, 'clipElement').name('Clip blocks').onChange(forceFullRedraw);
 timelineGui.add(timeline.options, 'flattenRowElements').name('Flat data tree').onChange(forceFullRedraw);
-timelineGui.add(timeline.options, 'rowHeight', 10, 50).step(1).name('Row height').onChange(forceFullRedraw);
-timelineGui.add(timeline.options, 'rowPadding', 0, 10).step(1).name('Row padding').onChange(forceFullRedraw);
-timelineGui.add(timeline.options, 'xAxisHeight', 30, 100).step(1).name('X axis height').onFinishChange(forceFullRedraw);
-timelineGui.add(timeline.options, 'yAxisWidth', 30, 100).step(1).name('Y axis width').onFinishChange(forceFullRedraw);
+timelineGui.add(timeline.options, 'clipElement').name('Clip blocks').onChange(forceFullRedraw);
+
+dimensionsGui.add(timeline.options, 'rowHeight', 10, 50).step(1).name('Row height').onChange(forceFullRedraw);
+dimensionsGui.add(timeline.options, 'rowPadding', 0, 10).step(1).name('Row padding').onChange(forceFullRedraw);
+dimensionsGui.add(timeline.options, 'xAxisHeight', 30, 100).step(1).name('X axis height').onFinishChange(forceFullRedraw);
+dimensionsGui.add(timeline.options, 'yAxisWidth', 30, 100).step(1).name('Y axis width').onFinishChange(forceFullRedraw);
+
+cullingGui.add(timeline.options, 'cullingX').name('X culling');
+cullingGui.add(timeline.options, 'cullingY').name('Y culling');
+cullingGui.add(timeline.options, 'cullingTolerance', -10, 10).step(1).name('Y culling distance');
+
+elementGui.add(timeline.options, 'alignLeft').name('Keep text visible');
+elementGui.add(timeline.options, 'alignOnTranslate').name('Keep text visible on translate');
 
 timeline
     .toggleDrawing(false)
